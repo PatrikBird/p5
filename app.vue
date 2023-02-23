a59ce18a557607119bcd0780fbed506ee4e15b46
Patrik <52401692+PatrikBird@users.noreply.github.com>
2023-02-23 17:22:03 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-white dark:bg-black">
+    <!-- <NuxtWelcome /> -->
+    <div>
+      <ColorModeSwitch />
+      <Icon name="mdi:check" class="text-green-500 dark:text-blue-500 w-10 h-10" />
+    </div>
+  </div>
+</template>
---
+++ components/ColorModeSwitch.vue
@@ -0,0 +1,10 @@
+<template>
+  <button
+    aria-label="Color Mode"
+    class="action-btn w-12 h-12"
+    @click="toggleDark()"
+  >
+    <Icon v-if="isDark" name="line-md:moon" />
+    <Icon v-else name="line-md:sunny-outline-loop" />
+  </button>
+</template>
